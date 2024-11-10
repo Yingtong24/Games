2f8ce11fed68d6a218fe49556444d7b622421dbe
Yingtong24 <yingtonghuo0524@gmail.com>
2024-11-10 18:57:26 +0000

    Add files via upload

--- Join the Duck/JtD.py
+++ Join the Duck/JtD.py
@@ -11,22 +11,44 @@ st=0
 tt=0
 td=10
 cds=0
+dot=0
 
 #Adding Actors
 def duckarmy():
     global st
     for i in range(td):
         duck=Actor("duck.jpg")
-        duck.pos=random.randint(10,990), random.randint(10,840)
+        duck.pos=random.randint(50,900), random.randint(50,800)
         ducks.append(duck)
     st=time.time()
 
 #Draw
 def draw():
     screen.blit("pond.jpg",(0,0))
+    count=1
     for i in ducks:
         i.draw()
+        screen.draw.text(str (count), (i.pos[0], i.pos[1]+50))
+        count+=1
+    for line in lines:
+        screen.draw.line(line[0], line[1], (255,207,248))
+    
 def update():
     pass
 duckarmy()
+
+def on_mouse_down(pos):
+    global dot, lines
+    if dot < 10:
+        if ducks[dot].collidepoint(pos):
+            if dot > 0:
+                lines.append((ducks[dot-1].pos, ducks[dot].pos))
+            dot+=1
+
+
+
+
+
+
+
 pgzrun.go()
