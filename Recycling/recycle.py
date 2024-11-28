7961e8ee2655a539713589012f0d773ec87ae851
Yingtong24 <yingtonghuo0524@gmail.com>
2024-11-28 19:13:25 +0000

    Update recycle.py

--- Recycling/recycle.py
+++ Recycling/recycle.py
@@ -2,5 +2,11 @@ import pgzrun
 import random
 WIDTH=1000
 HEIGHT=850
-
-pgzrun.go()
+#Variables
+lvs=5
+speed=10
+gover=False
+gc=False
+lv1=1
+items=[]
+ITEMS=["Battery.png", "Chemical.png", "Fire.png", "Plastic bag.png", "Paper bag.png"]
