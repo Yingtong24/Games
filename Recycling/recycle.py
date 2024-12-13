b8db60f6fa766526b3ef3fab138ce5f179100308
Yingtong24 <yingtonghuo0524@gmail.com>
2024-12-13 18:56:04 +0000

    Update recycle.py

--- Recycling/recycle.py
+++ Recycling/recycle.py
@@ -16,6 +16,10 @@ def draw():
         
 def update():
     pass
+    global items, lv1
+    if len(items) == 0:
+        items=objects(lv1)
+
 def objects(extra):
     newitems=[]
     options=["paperbag.png"]+random.choice(ITEMS, k=extra)
@@ -25,25 +29,29 @@ def objects(extra):
         newitems.append(item)
         gap_size = WIDTH / (len(newitems) +1)
         #Len is to find the number of items
+    for index, item in enumerate(newitems):
+        item.x = (index + 1)*gap_size
+        item.y = 0
+        animate(item, duration = speed - lv1, y = HEIGHT)
+    return newitems
 
+def gameover():
+    global gover
+    gover=True
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def on_mouse_down(pos):
+    global items, lv1, lvs, gc
+    for item in items:
+        if item.collidepoint(pos):
+            if "paperbag" in item.image:
+                if lv1 == lvs:
+                    gc=True
+                else:
+                    lv1+=1
+                    items.clear
+            else:
+                gameover()
+            
 
 
 
