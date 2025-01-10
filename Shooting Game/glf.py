d2ac980e3f93b453e4cc17eede59f4daffc92f77
Yingtong24 <yingtonghuo0524@gmail.com>
2025-01-10 19:10:49 +0000

    Update glf.py

--- Shooting Game/glf.py
+++ Shooting Game/glf.py
@@ -1,6 +1,6 @@
 import pgzrun
 import random
-WIDTH=1000
+WIDTH=1388
 HEIGHT=850
 #Variables
 color=(154, 245, 178)
@@ -10,15 +10,49 @@ enemies=[]
 score=0
 #Actors
 gingy=Actor("gingy.png")
-gingy.pos=()
+gingy.pos=(694,750)
 lordf=Actor("lord_farquaad.png")
+lordf.pos=random.randint(50,1338), 100
+def on_key_down(key):
+    if key==keys.SPACE:
+        bullets.append(Actor("bullet.png"))
+        bullets[-1].x = gingy.x
+        bullets[-1].y = gingy.y
+
 def draw():
+    screen.blit("castle.jpg", (0,0))
     gingy.draw()
     lordf.draw()
+    for i in bullets:
+        i.draw()
+    screen.draw.text("Score:"+str(score), (10,10), fontsize=30)
 
-
-
-
+def update():
+    global score
+    pass
+    if keyboard.left:
+        gingy.x -= 10
+        if gingy.x<0:
+            gingy.x=0
+    elif keyboard.right:
+        gingy.x += 10
+        if gingy.x>1388:
+            gingy.x=1388
+    lordf.y += 5
+    if lordf.y>850:
+        lordf.y=0
+        lordf.x=random.randint(0,1388)
+    for i in bullets:
+        if i.y <=0:
+            bullets.remove(i)
+        else:
+            i.y -= 20
+    for i in bullets:
+        if lordf.colliderect(i):
+            score+=1
+            bullets.remove(i)
+            lordf.y=0
+            lordf.x=random.randint(0,1388)
 
 
 pgzrun.go()
