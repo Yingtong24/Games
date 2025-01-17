fd42c7ec8793599503cf49182a1a001c7a415c37
Yingtong24 <yingtonghuo0524@gmail.com>
2025-01-17 17:30:28 +0000

    Update glf.py

--- Shooting Game/glf.py
+++ Shooting Game/glf.py
@@ -13,6 +13,8 @@ gingy=Actor("gingy.png")
 gingy.pos=(694,750)
 lordf=Actor("lord_farquaad.png")
 lordf.pos=random.randint(50,1338), 100
+shrek=Actor("shrek.png")
+shrek.pos=random.randint(50,1338), 300
 def on_key_down(key):
     if key==keys.SPACE:
         bullets.append(Actor("bullet.png"))
@@ -23,6 +25,7 @@ def draw():
     screen.blit("castle.jpg", (0,0))
     gingy.draw()
     lordf.draw()
+    shrek.draw()
     for i in bullets:
         i.draw()
     screen.draw.text("Score:"+str(score), (10,10), fontsize=30)
@@ -54,5 +57,20 @@ def update():
             lordf.y=0
             lordf.x=random.randint(0,1388)
 
-
+    shrek.y += 7
+    if shrek.y>850:
+        shrek.y=0
+        shrek.x=random.randint(0,1388)
+    for i in bullets:
+        if i.y <=0:
+            bullets.remove(i)
+        else:
+            i.y -= 20
+    for i in bullets:
+        if shrek.colliderect(i):
+            score+=2
+            bullets.remove(i)
+            shrek.y=0
+            shrek.x=random.randint(0,1388)
+    
 pgzrun.go()
